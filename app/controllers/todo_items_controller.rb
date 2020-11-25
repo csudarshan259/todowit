3a5938a9f14d36025611052d8d364a9f40cefcbb
Sudarshan <csudarshan259@gmail.com>
2020-11-25 23:34:47 +0530

    completed basic functionality

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
     before_action :set_todo_list
+    before_action :set_todo_item, except: [:create]
 
     def create
         @todo_item = @todo_list.todo_items.create(todo_item_params)
@@ -17,6 +18,14 @@ class TodoItemsController < ApplicationController
 
         
     end
+
+    def complete
+        @todo_item.update_attribute(:completed_at,Time.now)
+        redirect_to @todo_list, notice: "Todo item completed"
+
+        
+    end
+    
     
     private
 
@@ -29,6 +38,9 @@ class TodoItemsController < ApplicationController
         params[:todo_item].permit(:content)
         
     end
-
+    
+    def set_todo_item
+        @todo_item = @todo_list.todo_items.find(params[:id])
+    end
 
 end
